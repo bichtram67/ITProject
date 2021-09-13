fe7367969fc10f167f491c53f4f224927485564a
bichtram67 <tramnguyen672001@gmail.com>
2021-09-13 10:11:54 +0700

    Add project

---
+++ js/owlcarousel.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('#main-slider').owlCarousel({
+        items:1,
+        loop:true,
+        nav:true,
+        navText:['<span><i class="fas fa-chevron-left"></i></span>','<span><i class="fas fa-chevron-right"></i></span>'],
+        
+    })
+});
